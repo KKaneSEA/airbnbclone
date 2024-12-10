43c81fff568076ace7f1a8356db8e41e3d10f04f
K Kane <kane.kayla@gmail.com>
2024-12-09 18:34:28 -0800

    course

---
+++ app/actions/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import UsersList from "@/components/UsersList";
+import Form from "@/components/Form";
+
+function ActionsPage() {
+  return (
+    <>
+      <Form />
+      <UsersList />
+    </>
+  );
+}
+
+export default ActionsPage;
